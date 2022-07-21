6ed5877576ec4ead5fef11eaa7073328f5375b7a
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-07-21 15:31:28 +0100

    Added basic look for calculator and basic working functions

---
+++ app.js
@@ -0,0 +1,37 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a ,b) => {
+    return a / b;
+};
+
+const operate = (operator, a, b) => {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a,b);
+            break;
+        case '*':
+            result = multiply(a,b);
+            break;
+        case '/':
+            result = divide(a,b);
+            break;
+    }
+    
+    return result;
+};
+
+console.log(operate('*', 2, 5));
