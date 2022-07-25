ce1cf60699a138fedfac4bfbb39074cab042dd77
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-07-25 19:34:47 +0100

    Finished calculator application

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// This class will store all the values, store what operation will be used  
+// This class will store all the values and store what operations will be used  
 class Calculator {
     constructor(prevValue, currentValue) {
         this.prevValueText = prevValue;
@@ -14,7 +14,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentValue = this.currentValue.toString().slice(0, -1);
     }
 
     appendNumber(number) {
@@ -31,6 +31,12 @@ class Calculator {
         if (this.currentValue === '') {
             return; 
         }
+        // Checks to see if the previous value does contain a number, which if it does, run compute
+        // This allows us to continually chain numbers and operators together 
+        if (this.prevValue !== '') {
+            this.compute();
+        }
+
         // Sets the operator to whatever button was clicked
         this.operation = operation;
         // Sets the prevValue variable as the currentValue
@@ -40,12 +46,53 @@ class Calculator {
     }
 
     compute() {
+        let result;
+        const prev = parseFloat(this.prevValue);
+        const current = parseFloat(this.currentValue);
+        
+        // Checks to see if the previous and current value contain data
+        if(isNaN(prev) || isNaN(current)) {
+            return;
+        }
 
+        switch(this.operation) {
+            case '+':
+                result = prev + current;
+                break;
+            case '-':
+                result = prev - current;
+                break;
+            case 'x':
+                result = prev * current;
+                break;
+            case '/':
+                result = prev / current;
+                break;
+            default:
+                return;
+        }
+
+        // Makes sure that user cannot divide by 0, and displays error message
+        if(this.operation = '/' && this.currentValue == 0) {
+            result = 'Cannot divide by 0';
+        }
+
+        // This shows the result of the computation 
+        this.currentValue = result;
+
+        // Resetting the operation and previous value but not current value as it can be used for the next operation
+        this.operation = undefined;
+        this.prevValue = '';
     }
 
     updateDisplay() {
         this.currentValueText.innerText = this.currentValue;
-        this.prevValueText.innerText = this.prevValue;
+        // If the user has clicked a operation button, then it will show on the display
+        if(this.operation != null) {
+            this.prevValueText.innerText = `${this.prevValue} ${this.operation}`;
+        } else {
+            this.prevValueText.innerText = '';
+        }
     }
 }
 
@@ -58,6 +105,7 @@ const currentValueText = document.querySelector('.value');
 const prevValueText = document.querySelector('.typed-value');
 
 // Defining the class created above
+// Now, when using calculator object, we can use dot notation, to access any function
 const calculator = new Calculator(prevValueText, currentValueText);
 
 
@@ -75,7 +123,20 @@ opBtns.forEach(btn => {
     });
 });
 
+equalBtn.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
 
+clearBtn.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteBtn.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
 
 
 
